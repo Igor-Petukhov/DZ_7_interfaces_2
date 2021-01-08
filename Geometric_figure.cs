e07b450b5ffe061e284d61f0efd4d2cd02550a02
kapandaiiine <kapandaiiine@gmail.com>
2021-01-08 08:14:39 +0200

    Add project files.

---
+++ Geometric_figure.cs
@@ -0,0 +1,326 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    interface IFigure
+    {
+        void Print();
+    }
+
+    abstract class Geometric_figure
+    {
+        protected float square;
+        protected float perimeter;
+
+        public abstract float Square();
+        public abstract float Perimeter();
+
+        public void PrintSquareText()
+        {
+            Console.Write($"Площадь фигуры ");
+        }
+
+        public void PrintPerimeterText()
+        {
+            Console.Write($"Периметр фигуры ");
+        }
+
+
+    }
+
+    class Triangle : Geometric_figure, IFigure
+    {
+        int x1, y1, x2, y2, x3, y3;
+        float AB, BC, AC;
+
+        public Triangle(int x1, int y1, int x2, int y2, int x3, int y3)
+        {
+            //Проверка треугольника на существование
+            AB = (float)Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            BC = (float)Math.Sqrt(Math.Pow(x3 - x2, 2) + Math.Pow(y3 - y2, 2));
+            AC = (float)Math.Sqrt(Math.Pow(x3 - x1, 2) + Math.Pow(y3 - y1, 2));
+            if ((AB + BC <= AC) || (BC + AC <= AB) || (AC + AB <= BC))
+                Console.WriteLine("Извините такого треугольника не существует. Возможно :)");
+            else
+            {
+                x1 = x1;
+                y1 = y1;
+                x2 = x2;
+                y2 = y2;
+                x3 = x3;
+                y3 = y3;
+            }
+        }
+        public override float Square()
+        {
+            float p = (AB + BC + AC) / 2; //Полупериметр
+            return (float)Math.Sqrt(p * (p - AB) * (p - BC) * (p - AC));
+        }
+
+        public override float Perimeter()
+        {
+            return AB + BC + AC;
+        }
+
+        public void Print()
+        {
+            float tmp = AB;
+            for (int i = 0; i < (int)AC; i++)
+            {
+                for (int j = 0; j < tmp; j++)
+                {
+                    Console.Write("*");
+                }
+                tmp -= AB / AC;
+                Console.WriteLine();
+            }
+        }
+    }
+
+
+    class SquareFigure : Geometric_figure, IFigure
+    {
+        float AB;
+        public SquareFigure(float AB) //получим длину стороны квадрата
+        {
+            this.AB = AB;
+        }
+        public override float Square()
+        {
+            return AB * AB;
+        }
+
+        public override float Perimeter()
+        {
+            return 4 * AB;
+        }
+
+        public void PrintSquare()
+        {
+            base.PrintSquareText();
+            Console.WriteLine($"квадрат: {Square()}");
+        }
+
+        public void PrintPerimeter()
+        {
+            base.PrintPerimeterText();
+            Console.WriteLine($"квадрат: {Perimeter()}");
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < AB; i++)
+            {
+                for (int j = 0; j < AB; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+
+    class Rhombus : Geometric_figure, IFigure
+    {
+        float AC, BD; //диагонали
+
+        public Rhombus(float AC, float BD) //по двум диагоналям
+        {
+            this.AC = AC;
+            this.BD = BD;
+        }
+        public override float Perimeter()
+        {
+            return (float)(2 * (Math.Sqrt(Math.Pow(AC, 2) + Math.Pow(BD, 2))));
+        }
+
+        public override float Square()
+        {
+            return AC * BD / 2;
+        }
+
+        public void PrintSquare()
+        {
+            base.PrintSquareText();
+            Console.WriteLine($"ромб: {Square()}");
+        }
+
+        public void PrintPerimeter()
+        {
+            base.PrintPerimeterText();
+            Console.WriteLine($"ромб: {Perimeter()}");
+        }
+
+        public void Print()
+        {
+            float tmp = (float)(AC / 2);
+            for (int i = 0; i < (float)(BD/2); i++)
+            {
+                for (int j = 0; j < (float)(tmp); j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = (int)(tmp); j < (float)AC /2; j++)
+                {
+                    Console.Write("2");
+                }
+                for (int j = (int)(tmp); j < (float)AC / 2; j++)
+                {
+                    Console.Write("3");
+                }
+                tmp -= (float)(AC / 2) / (float)(BD / 2);
+                Console.WriteLine();
+            }
+            float tmp2 = (float)(AC / 2);
+            for (int i = (int)(BD / 2); i > 0; i--)
+            {
+                for (int j = 0; j < (AC/2) - tmp2; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 0; j < tmp2; j++)
+                {
+                    Console.Write("4");
+                }
+                for (int j = 0; j < tmp2; j++)
+                {
+                    Console.Write("5");
+                }
+                tmp2 -= (float)(AC / 2) / (float)(BD / 2);
+                Console.WriteLine();
+            }
+        }
+    }
+
+    class Rectangle : Geometric_figure, IFigure
+    {
+        float AB, AD; //через 2 стороны
+        public Rectangle(float AB, float AD)
+        {
+            this.AB = AB;
+            this.AD = AD;
+        }
+
+        public override float Perimeter()
+        {
+            return 2 * AB + 2 * AD;
+        }
+        public override float Square()
+        {
+            return AB * AD;
+        }
+
+        public void PrintSquare()
+        {
+            base.PrintSquareText();
+            Console.WriteLine($"прямоугольник: {Square()}");
+        }
+
+        public void PrintPerimeter()
+        {
+            base.PrintPerimeterText();
+            Console.WriteLine($"прямоугольник: {Perimeter()}");
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < (int)AD; i++)
+            {
+                for (int j = 0; j < (int)AB; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+
+    class SostavnFigura : Geometric_figure, IFigure
+    {
+        private Geometric_figure[] gfArr;
+
+        public SostavnFigura(Geometric_figure[] arrIn)
+        {
+            gfArr = new Geometric_figure[arrIn.Length];
+            for (int i = 0; i < arrIn.Length; i++)
+            {
+                gfArr[i] = arrIn[i];
+            }
+        }
+        //public void Add_figure(int x1, int y1, int x2, int y2, int x3, int y3) //triangle
+        //{
+        //    n++;
+        //    Geometric_figure[] tmp = new Geometric_figure[n];
+        //    Array.Copy(gfArr, tmp, n - 1);
+        //    tmp[n-1] = new Triangle(x1, y1, x2, y2, x3, y3);
+        //    gfArr = tmp;
+        //    //lst.Add(triangle);
+        //}
+        //public void Add_figure(float AB) //square figure
+        //{
+        //    n++;
+        //    Geometric_figure[] tmp = new Geometric_figure[n];
+        //    Array.Copy(gfArr, tmp, n - 1);
+        //    tmp[n-1] = new SquareFigure(AB);
+        //    gfArr = tmp;
+        //}
+        //public void Add_figure(float a, float b, int figure_id)//figure_id 1 - Rhombus, 2 - Rectangle
+        //{
+        //    switch (figure_id)
+        //    {
+        //        case 1: //rhombus
+        //            {
+        //                n++;
+        //                Geometric_figure[] tmp = new Geometric_figure[n];
+        //                Array.Copy(gfArr, tmp, n - 1);
+        //                tmp[n-1] = new Rhombus(a, b);
+        //                gfArr = tmp;
+        //                break;
+        //            }
+        //        case 2: //rectangle
+        //            {
+        //                n++;
+        //                Geometric_figure[] tmp = new Geometric_figure[n];
+        //                Array.Copy(gfArr, tmp, n - 1);
+        //                tmp[n-1] = new Rectangle(a, b);
+        //                gfArr = tmp;
+        //                break;
+        //            }
+        //        default:
+        //            break;
+        //    }
+
+        //}
+        public override float Perimeter()
+        {
+            float tmp = 0;
+            for (int i = 0; i < gfArr.Length; i++)
+            {
+                tmp += gfArr[i].Perimeter();
+            }
+            return tmp;
+        }
+
+        public void Print()
+        {
+            foreach (Geometric_figure item in gfArr)
+            {
+                (item as IFigure).Print();
+            }
+        }
+
+        public override float Square()
+        {
+            float tmp = 0;
+            for (int i = 0; i < gfArr.Length; i++)
+            {
+                tmp += gfArr[i].Square();
+            }
+            return tmp;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangle ABC = new Triangle(0, 10, 7, 10, 0, 0);
+            ABC.Print();
+
+            SquareFigure ABCD = new SquareFigure(10);
+            ABCD.Print();
+
+            Rhombus rhombus = new Rhombus(12, 6);
+            rhombus.Print();
+
+            Rectangle rectangle = new Rectangle(12, 6);
+            rectangle.Print();
+
+
+            Geometric_figure[] gfArr = new Geometric_figure[4]
+            {
+            ABC,
+            ABCD,
+            rhombus,
+            rectangle,
+            };
+            SostavnFigura sf = new SostavnFigura(gfArr);
+            sf.Print();
+
+
+            Console.ReadKey();
+        }
+    }
+}
